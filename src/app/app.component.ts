b0c2b089341aac85b2238bdb0355f8b6adfb2fce
mcruzlp <mcruzlp@gmail.com>
2022-01-31 23:24:16 +0100

    blockUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { BlockUIModule } from 'primeng/blockui';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,16 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  blockedPanel: boolean = false;
+
+  blockedDocument: boolean = false;
+
+  blockDocument() {
+    this.blockedDocument = true;
+    setTimeout(() => {
+      this.blockedDocument = false;
+    }, 3000);
+  }
   constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-
+import { BlockUIModule } from 'primeng/blockui';
+import { PanelModule } from 'primeng/panel';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, ButtonModule,
+    FormsModule,
+    ButtonModule,
     InputTextModule,
+    BlockUIModule,
+    PanelModule,
+    RippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
